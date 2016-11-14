3c100c07618dbf0d41a786ea164a38d302420ce8
C0D1NGD0J0 <web-dev@United4Life.local>
2016-11-14 12:27:04 +0100

    hold button added

--- app_v4.js
+++ app_v4.js
@@ -7,12 +7,10 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-*/
 
-
-/* VERSION 3 GOALS
-	-> Using ternary operator
-	-> Add, remove and toggle html classes
+	VERSION 4 GOALS
+	-> Refactoring
+	-> Working on the hold button	
 */
 
 var scores, roundScore, activePlayer, dice;
@@ -43,21 +41,32 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
 		// Next player
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
-
-		document.getElementById('current-0').textContent = '0';
-		document.getElementById('current-1').textContent = '0';
-
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
-		
-		document.querySelector('.dice').style.display = 'none';
+		nextPlayer();
 	}
-})
+});
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+	// Add current score to global score
+	scores[activePlayer] += roundScore;
 
+	// Update UI
+	document.getElementById('#score-' + activePlayer).textContent = scores[activePlayer];
 
+	// Check if player won the game
+	nextPlayer();
+});
 
+function nextPlayer(){
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+	
+	document.querySelector('.dice').style.display = 'none';
+}
 
 
