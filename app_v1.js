3e313bad6f66784bef1740497e2951c0bfafd0b7
C0D1NGD0J0 <web-dev@United4Life.local>
2016-11-14 11:17:20 +0100

    version 1 complete

--- app_v1.js
+++ app_v1.js
@@ -18,3 +18,13 @@ GAME RULES:
 	-> Changing css styles via js
 */
 
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
