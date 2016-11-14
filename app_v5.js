b58d90c641b7a0aeb670e91820f441d54873eb73
C0D1NGD0J0 <web-dev@United4Life.local>
2016-11-14 13:53:45 +0100

    initialize method created + version 5 completed

--- app_v5.js
+++ app_v5.js
@@ -7,25 +7,13 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-
-	VERSION 4 GOALS
-	-> Refactoring
-	-> Working on the hold button	
+	VERSION 5 GOALS
+	-> implement 'New Game' functionality
 */
 
 var scores, roundScore, activePlayer, dice;
+init();
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 	let dice = Math.floor(Math.random() * 6) + 1;
@@ -75,4 +63,29 @@ function nextPlayer(){
 	document.querySelector('.dice').style.display = 'none';
 };
 
+document.querySelector('.btn-new').addEventListener('click', init);
 
+function init(){
+	scores = [0, 0];
+	activePlayer = 0;
+	roundScore = 0;
+
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+
+	document.querySelector('.player-0-panel').classList.add('active');
+}
